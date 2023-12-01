4bab52c3d266a11d5db545b9873aa05919cc73ff
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-01 21:04:38 +0800

    q1

--- q1/q1.py
+++ q1/q1.py
@@ -1,4 +1,13 @@
-fh = open("input.txt", "r")
-fhread = fh.readlines(fh)
-print(fhread[0])
+import re
 
+fh = open("/Users/lavanya/Documents/GitHub/advent_of_code/q1/input.txt", "r")
+fhread = fh.readlines()
+
+sum = 0
+
+for i in fhread:
+    nums = re.findall("\d", i)
+    number = int(nums[0])*10 + int(nums[-1])
+    sum = sum + number
+
+print(sum)
