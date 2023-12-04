e0890f14f458a84e4953f80873d80af77088c928
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-04 13:56:45 +0800

    q4 added

--- q4/part1.py
+++ q4/part1.py
@@ -1,13 +1,27 @@
 fh = open("/Users/lavanya/Documents/GitHub/advent_of_code_2023/q4/input.txt", "r")
 fhread = fh.readlines()
 
-print(fhread)
+total = 0
 
 for i in fhread:
     nums = i.split(":")[1]
     numsplit = nums.split("|")
     win_nums = numsplit[0]
     my_nums = numsplit[1]
-    print(win_nums)
+    all_win = win_nums.split(" ")
+    all_my = my_nums.split(" ")
+
+    score = 0
+    
+    for k in all_my:
+        k = k.rstrip("\n")
+        if k in all_win and k != "":
+            if score == 0:
+                score = 1
+            else:
+                score *= 2
+    total += score
+
+print(total)
 
 fh.close()
--- q4/part2.py
+++ q4/part2.py
@@ -1,4 +1,49 @@
 fh = open("/Users/lavanya/Documents/GitHub/advent_of_code_2023/q4/input.txt", "r")
 fhread = fh.readlines()
 
+total = 0
+d = {}
+match = {}
+
+for i in fhread:
+    cards = i.split(":")[0]
+    card_num = int(cards.split(" ")[-1])
+    d[card_num] = 1
+
+    nums = i.split(":")[1]
+    numsplit = nums.split("|")
+    win_nums = numsplit[0]
+    my_nums = numsplit[1]
+    all_win = win_nums.split(" ")
+    all_my = my_nums.split(" ")
+
+    score = 0
+    
+    for k in all_my:
+        k = k.rstrip("\n")
+        if k in all_win and k != "":
+            score = score + 1
+    match[card_num] = score
+
+
+for i in d:
+    matches = match[i]
+    if matches != 0:
+        for m in range(d[i]):
+                k = i+1
+                for j in range(matches):
+                    if k in d:
+                        d[k] += 1
+                        k = k + 1
+                    else:
+                        break
+
+
+total = 0
+
+for l in d:
+    total += d[l]
+
+print(total)
+
 fh.close()
